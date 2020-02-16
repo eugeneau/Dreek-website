a91f4ef7143cd13d2247067326ff73f993c99bcf
eugeneau <46308541+eugeneau@users.noreply.github.com>
2020-02-16 07:06:41 -0700

    Delete .js

--- .js
+++
@@ -1,9 +0,0 @@
-  function checkPassword() {
-   var password = document.getElementById("passwordBox");
-   var passwordText = password.value;
-   if(passwordText == "cindersandcoal") {
-    return true;
-   }
-   alert("Incorrect password!");
-   return false;
-   }
