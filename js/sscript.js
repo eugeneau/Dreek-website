e2f9c9397ac51acea21472962d2de125dd671096
eugeneau <46308541+eugeneau@users.noreply.github.com>
2020-03-23 16:25:31 -0600

    Update sscript.js

--- js/sscript.js
+++ js/sscript.js
@@ -2,8 +2,9 @@
    var password = document.getElementById("passwordBox");
    var passwordText = password.value;
    if(passwordText == "Mitrz") {
-    window.location.assign("https://dreek16designer.com/secret/Meetourz/secret.bungpouhaihg.zilch");
-    return true;
+     alert("Correct!");
+     window.location.assign("https://dreek16designer.com/secret/Meetourz/secret.bungpouhaihg.zilch");
+     return true;
    }
    alert("Incorrect password!");
    return false;
