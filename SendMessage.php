7feb56ddd087d9f77b3aff1b9c680d750d854e51
eugeneau <46308541+eugeneau@users.noreply.github.com>
2020-05-12 17:24:01 -0600

    Add files via upload

---
+++ SendMessage.php
@@ -0,0 +1,21 @@
+<?
+    $to = "dreek16designer@outlook.com";
+	$from = "dreek16@yahoo.com";
+	$subject = "A message from Dreek16designer.com";
+	
+	$headers  = "From: $from\r\n";
+	$headers .= "Content-type: text/html\r\n";
+
+	$message = <<<EOF
+	    <html>
+		    <body>
+			<h1>Email from Dreek16designer.com</h1>
+			<a href="https://dreek16designer.com/">Back to the website</a>
+			</body>
+	    </html>
+	EOF;
+
+	mail($to, $subject, $message, $headers);
+
+	echo "Successfully submitted!";
+?>
