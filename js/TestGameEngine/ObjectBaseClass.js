07e556c32c06ad33bb8d3122f383c380d9360b84
eugeneau <46308541+eugeneau@users.noreply.github.com>
2020-06-07 16:41:16 -0600

    Update ObjectBaseClass.js

--- js/TestGameEngine/ObjectBaseClass.js
+++ js/TestGameEngine/ObjectBaseClass.js
@@ -7,3 +7,15 @@ function Loop() {
 function startGame() {
     
 }
+var gravity = -9.81;
+
+function Gravity() {
+    //apply gravity
+    this.y = this.y = gravity;
+    setInterval(Loop, 1);
+}
+
+
+startGame();
+setInterval(Loop, 1);
+setInterval(Gravity, 1);
