f87715ed1b981db64c85e4413e37a75745ebcf6b
eugeneau <46308541+eugeneau@users.noreply.github.com>
2020-06-07 16:47:44 -0600

    Update ObjectBaseClass.js

--- js/TestGameEngine/ObjectBaseClass.js
+++ js/TestGameEngine/ObjectBaseClass.js
@@ -15,6 +15,22 @@ function Gravity() {
     setInterval(Loop, 1);
 }
 
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.angle = 0;
+  this.x = x;
+  this.y = y;
+  this.update = function() {
+    ctx = myGameArea.context;
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.fillStyle = color;
+    ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
+    ctx.restore();
+  }
+}
 
 startGame();
 setInterval(Loop, 1);
