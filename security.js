d87965a621244960e71564b33d917b2821f6df51
eugeneau <46308541+eugeneau@users.noreply.github.com>
2020-07-17 13:34:29 -0600

    Update and rename js/sscript.js to security.js

--- security.js
+++ security.js
@@ -1,9 +1,11 @@
 var url = "https://example.com";
+var correctAnswer;
 
 function checkPassword() {
    var password = document.getElementById("passwordBox");
+   correctAnswer = document.getElementById("answer");
    var passwordText = password.value;
-   if(passwordText == password.correctAnswer) {
+   if(passwordText == correctAnswer) {
      alert("Correct");
      window.location.assign(url);
      return true;
